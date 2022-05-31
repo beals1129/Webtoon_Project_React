86090c9f5d1564329363aa280e625e7e1d74f503
beals1129 <beals1129@gmail.com>
2022-05-31 13:46:14 +0900

    20220531

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
             <div>
                 <a><img/></a>
                 <input></input>
-                <button>Language2</button>
+                <button>Language</button>
                 <button>Login</button>
             </div> 
             <SideBar></SideBar>
